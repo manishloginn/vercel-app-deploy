5f92349e0b63fd1dfbf52cfb7576d99b7b27b962
Manish <132683386+manishloginn@users.noreply.github.com>
2024-08-20 11:27:29 +0000

    second

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
+const port = 5000
 
 
 app.get("/" , (req, res) => {
     res.send("homepage")
+    console.log('starrt')
+})
+
+app.listen(port, () => {
+    console.log('runnning')
 })
 
